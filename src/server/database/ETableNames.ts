141e0ef407ef327d4663823420a4032428775c0f
Joaovjdev <zumbe25tt@gmail.com>
2023-11-08 17:43:30 -0300

    added cidades model and updated cidade controller

---
+++ src/server/database/ETableNames.ts
@@ -0,0 +1,5 @@
+
+
+export enum ETbaleNames {
+  cidade = 'cidade'
+};
---
+++ src/server/database/migrations/0000_create_cidade.ts
@@ -0,0 +1,30 @@
+import { Knex } from 'knex';
+import { ETbaleNames } from '../ETableNames';
+
+
+export async function up(knex: Knex) {
+  return knex
+  .schema
+  .createTable(ETbaleNames.cidade, table => {
+    table.bigIncrements('id').primary().index();
+    table.string('nome', 150).index().notNullable();
+
+    table.comment('Tabela para armazenar cidades do sistema');
+
+  })
+  .then(() => {
+    console.log(`Create table ${ETbaleNames.cidade}`)
+  });
+    
+}
+
+
+export async function down(knex: Knex) {
+  return knex.schema
+  .dropTable(ETbaleNames.cidade)
+  .then(() => {
+    console.log(`Dropped table ${ETbaleNames.cidade}`)
+  });
+
+}
+
