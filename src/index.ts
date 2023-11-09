1976dcafa26fffcadd9f1737d6464c257183d1e9
Joaovjdev <zumbe25tt@gmail.com>
2023-11-09 10:10:35 -0300

    feat: added seed file to cidades table

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,12 @@ const startServer = () => {
 
 if(process.env.IS_LOCALHOST !== 'true') {
 
-  Knex.migrate.latest()
+  Knex.migrate
+  .latest()
   .then(() => {
-    startServer();
+    Knex.seed.run()
+      .then(() => startServer())
+      .catch(console.log);
   })
   .catch(console.log);
 
