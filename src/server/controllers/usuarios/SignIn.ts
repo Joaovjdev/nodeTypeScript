8cad131c4ee7fe857bfc778db7dd818d16b1a11a
Joaovjdev <zumbe25tt@gmail.com>
2023-11-10 13:18:57 -0300

    feat: added authentication to cidades e pessoas endpoints

--- src/server/controllers/usuarios/SignIn.ts
+++ src/server/controllers/usuarios/SignIn.ts
@@ -6,6 +6,7 @@ import * as yup from 'yup';
 import { validation } from '../../shared/middlewares';
 import { IUsuario } from '../../database/models';
 import { UsuariosProvider } from '../../database/providers/cidades/usuarios';
+import { PasswordCrypto } from '../../shared/services';
 
 
 interface IBodyProps extends Omit<IUsuario, 'id' | 'nome'> { }
@@ -30,7 +31,10 @@ export const signIn = async (req: Request<{}, {}, IBodyProps>, res: Response) =>
       }
     });
   }
-  if (senha !== result.senha) {
+
+  const passwordMatch = await PasswordCrypto.verifyPassword(senha, result.senha);
+
+  if (!passwordMatch) {
     return res.status(StatusCodes.UNAUTHORIZED).json({
       errors: {
         default: 'Email ou senha invalidos'
--- src/server/shared/middlewares/index.ts
+++ src/server/shared/middlewares/index.ts
@@ -1,1 +1,2 @@
 export * from './validantion';
+export * from './EnsureAuthenticated';
