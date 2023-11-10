6754fe70045478afcf611e57fe70ec39993cbd57
Joaovjdev <zumbe25tt@gmail.com>
2023-11-10 07:47:33 -0300

    feat: added migrations to usuario table

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -2,7 +2,8 @@ import { Router } from "express";
 import { StatusCodes } from 'http-status-codes';
 
 
-import { CidadesController } from './../controllers';
+import { CidadesController, PessoasController } from './../controllers';
+
 
 const router = Router();
 
@@ -17,6 +18,12 @@ router.post('/cidades', CidadesController.createValidation, CidadesController.cr
 router.get('/cidades/:id', CidadesController.getByIdValidation, CidadesController.getById);
 router.put('/cidades/:id', CidadesController.updateByIdValidation, CidadesController.updateById);
 router.delete('/cidades/:id', CidadesController.deleteByIdValidation, CidadesController.deleteById);
+
+router.get('/pessoas', PessoasController.getAllValidation, PessoasController.getAll);
+router.post('/pessoas', PessoasController.createValidation, PessoasController.create);
+router.get('/pessoas/:id', PessoasController.getByIdValidation, PessoasController.getById);
+router.put('/pessoas/:id', PessoasController.updateByIdValidation, PessoasController.updateById);
+router.delete('/pessoas/:id', PessoasController.deleteByIdValidation, PessoasController.deleteById);
 
 
 
